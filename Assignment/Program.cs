37bf325bee2a64b03fd05e307d59b2f9d2db2c33
Aiswarya.Saju.ust.com <Aiswarya.Saju.ust.com>
2023-11-15 09:21:41 +0530

    14-11-2023

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,6 +1,22 @@
 ﻿using Assignment;
 using NUnit.Framework;
 
+//AmazonTests a = new AmazonTests();
+//a.InitializeChromeDriver();
+//try
+//{
+//    a.TitleTest();
+//    a.OrganizationTypeTest();
+
+//}
+
+//catch (AssertionException)
+//{
+//    Console.WriteLine("Fail");
+//}
+
+//a.Destruct();
+
 AmazonTests a = new AmazonTests();
 a.InitializeChromeDriver();
 try
@@ -16,3 +32,4 @@ catch (AssertionException)
 }
 
 a.Destruct();
+
