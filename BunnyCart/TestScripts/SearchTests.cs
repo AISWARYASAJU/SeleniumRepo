97edbf1c57bc4c0aefc94f402deada2a839b2813
Aiswarya.Saju.ust.com <Aiswarya.Saju.ust.com>
2023-11-26 09:44:15 +0530

    22-11-2023

--- BunnyCart/TestScripts/SearchTests.cs
+++ BunnyCart/TestScripts/SearchTests.cs
@@ -1,52 +1,52 @@
-﻿using BunnyCart.PageObjects;
-using BunnyCart.Utilities;
-using OpenQA.Selenium;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using BunnyCart.PageObjects;
+////using BunnyCart.Utilities;
+//using OpenQA.Selenium;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace BunnyCart.TestScripts
-{
-    [TestFixture]
-    internal class SearchTests : CoreCodes
-    {
-        [Test]
-        public void CreateAccountTest()
-        {
-            BunnyCartHomePage bchp = new(driver);
+//namespace BunnyCart.TestScripts
+//{
+//    [TestFixture]
+//    internal class SearchTests : CoreCodes
+//    {
+//        [Test]
+//        public void CreateAccountTest()
+//        {
+//            BunnyCartHomePage bchp = new(driver);
 
-            bchp.ClickCreateAccountLink();
-            Thread.Sleep(2000);
+//            bchp.ClickCreateAccountLink();
+//            Thread.Sleep(2000);
 
-            Assert.That(driver?.FindElement(By.XPath("//div[" +
-                "@class='modal-inner-wrap']//following::h1[2]")).Text,
-                Is.EqualTo("Create an Account"));
+//            Assert.That(driver?.FindElement(By.XPath("//div[" +
+//                "@class='modal-inner-wrap']//following::h1[2]")).Text,
+//                Is.EqualTo("Create an Account"));
 
-            string? currDir = Directory.GetParent(@"../../../")?.FullName;
-            string? excelFilePath = currDir + "/TestData/InputData.xlsx";
-            string? sheetName = "CreateAccount";
+//            string? currDir = Directory.GetParent(@"../../../")?.FullName;
+//            string? excelFilePath = currDir + "/TestData/InputData.xlsx";
+//            string? sheetName = "CreateAccount";
 
-            List<SignUp> excelDataList = ExcelUtils.ReadSignUpExcelData(excelFilePath, sheetName);
+//            List<SignUp> excelDataList = ExcelUtils.ReadSignUpExcelData(excelFilePath, sheetName);
 
-            foreach (var excelData in excelDataList)
-            {
+//            foreach (var excelData in excelDataList)
+//            {
 
-                string? firstName = excelData?.FirstName;
-                string? lastName = excelData?.LastName;
-                string? email = excelData?.Email;
-                string? pwd = excelData?.Password;
-                string? conpwd = excelData?.ConfirmPassword;
-                string? mbno = excelData?.MobileNumber;
+//                string? firstName = excelData?.FirstName;
+//                string? lastName = excelData?.LastName;
+//                string? email = excelData?.Email;
+//                string? pwd = excelData?.Password;
+//                string? conpwd = excelData?.ConfirmPassword;
+//                string? mbno = excelData?.MobileNumber;
 
-                Console.WriteLine($"First Name: {firstName}, Last Name: {lastName}, Email: {email}, Password: {pwd}, Confirm Password: {conpwd}, Mobile Number: {mbno}");
+//                Console.WriteLine($"First Name: {firstName}, Last Name: {lastName}, Email: {email}, Password: {pwd}, Confirm Password: {conpwd}, Mobile Number: {mbno}");
 
 
-                bchp.ClickCreateAccount(firstName, lastName, email, pwd, conpwd, mbno);
-                // Assert.That(""."")
+//                bchp.ClickCreateAccount(firstName, lastName, email, pwd, conpwd, mbno);
+//                // Assert.That(""."")
 
-            }
-            }
-        }
-}
+//            }
+//            }
+//        }
+//}
---
+++ NaptolASSIGNMENT/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace NaptolASSIGNMENT
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
